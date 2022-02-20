a618f286219db9ac2736b0443daff5bbfd7b2124
Jose Granado <josegrad@gmail.com>
2022-02-20 16:27:49 +0200

    Update connection.rb
    Logs request and response

--- lib/shopify_api/connection.rb
+++ lib/shopify_api/connection.rb
@@ -5,6 +5,8 @@ module ShopifyAPI
 
     module ResponseCapture
       def handle_response(response)
+        puts "*** SHOPIFY RESPONSE"
+        puts response.inspect
         @response = super(ShopifyAPI::MessageEnricher.new(response))
       end
     end
@@ -13,6 +15,10 @@ module ShopifyAPI
 
     module RequestNotification
       def request(method, path, *arguments)
+        puts "*** SHOPIFY REQUEST"
+        puts arguments
+        puts method
+        puts path
         super.tap do |response|
           notify_about_request(method, path, response, arguments)
         end
